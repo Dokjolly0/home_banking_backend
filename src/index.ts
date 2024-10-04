31b3c6d0800f139719703b443ebf4024c2ab17ce
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-10-04 23:54:28 +0200

    Change conn string

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import 'reflect-metadata';
-import app from './app';
-import mongoose from 'mongoose';
+import "reflect-metadata";
+import app from "./app";
+import mongoose from "mongoose";
 
-mongoose.set('debug', true);
-mongoose.connect('mongodb+srv://lucabenini:BHD4IYDPwu1gkGQ7@mybankingdb.fwk8n.mongodb.net/?retryWrites=true&w=majority&appName=myBankingDb')
-  .then(_ => {
-    console.log('Connected to db');
-    app.listen(3000, () => {
-      console.log('Server listening on port 3000');
+mongoose.set("debug", true);
+mongoose
+  .connect("mongodb+srv://Dokjolly:AleVio03@offertelavoro.aegmrwj.mongodb.net/")
+  .then((_) => {
+    console.log("Connected to db");
+    app.listen(3001, () => {
+      console.log("Server listening on port 3000");
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
-  })
+  });
